b7136423ad020c8e55bc2a000062f0305c2219a6
Lonnie VanZandt <lonniev@gmail.com>
2015-06-22 16:53:27 -0600

    repair guard checking

--- providers/computername.rb
+++ providers/computername.rb
@@ -13,43 +13,78 @@ end
 
 action :add do
 
-  converge_by("add new Fully Qualified Domain Name #{new_resource.host} for computer #{new_resource.name}") do
+  unless fqdn_added?( new_resource.host )
 
-    powershell_script "netdom computername" do
-      code <<-EOH
-        netdom computername #{new_resource.name} /add:#{new_resource.host}
-      EOH
+    converge_by("add new Fully Qualified Domain Name #{new_resource.host} for computer #{new_resource.name}") do
+
+      powershell_script "netdom computername" do
+        code <<-EOH
+          netdom computername #{new_resource.name} /add:#{new_resource.host}
+        EOH
+      end
+
+      Chef::Log.info("#{new_resource} added #{new_resource.host} for #{new_resource.name}")
     end
 
-    Chef::Log.info("#{new_resource} added #{new_resource.host} for #{new_resource.name}")
+    new_resource.updated_by_last_action(true)
   end
-
-  new_resource.updated_by_last_action(true)
 end
 
 action :add_primary do
 
-  unless new_resource.host.casecmp( activeFqdn() )
+  unless fqdn_added?( new_resource.host )
 
     converge_by("add new primary Fully Qualified Domain Name #{new_resource.host} for computer #{new_resource.name}") do
-  
+
       reboot "New ComputerName" do
         action :nothing
         reason "The new primary computername #{new_resource.host} needs a reboot to become effective."
       end
-      
+
       powershell_script "netdom computername" do
         code <<-EOH
           netdom computername #{new_resource.name} /add:#{new_resource.host}
           netdom computername #{new_resource.name} /makeprimary:#{new_resource.host}
         EOH
-        
+
         notifies new_resource.reboot_immediately ? :reboot_now : :request_reboot, 'reboot[New ComputerName]', :immediately
       end
-  
+
       Chef::Log.info("#{new_resource} added primary #{new_resource.host} for #{new_resource.name}")
     end
-    
+
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+action :make_primary do
+
+  unless fqdn_active?( new_resource.host )
+
+    converge_by("make primary Fully Qualified Domain Name #{new_resource.host} for computer #{new_resource.name}") do
+
+      reboot "New ComputerName" do
+        action :nothing
+        reason "The new primary computername #{new_resource.host} needs a reboot to become effective."
+      end
+
+      powershell_script "netdom computername" do
+        code <<-EOH
+          netdom computername #{new_resource.name} /add:#{new_resource.host}
+        EOH       
+      end unless fqdn_added?( new_resource.host )
+      
+      powershell_script "netdom computername" do
+        code <<-EOH
+          netdom computername #{new_resource.name} /makeprimary:#{new_resource.host}
+        EOH
+
+        notifies new_resource.reboot_immediately ? :reboot_now : :request_reboot, 'reboot[New ComputerName]', :immediately
+      end
+
+      Chef::Log.info("#{new_resource} made primary #{new_resource.host} for #{new_resource.name}")
+    end
+
     new_resource.updated_by_last_action(true)
   end
 end
@@ -59,12 +94,32 @@ def registry_get_values(key_path, architecture = :machine)
   registry.get_values(key_path)
 end
 
-def activeFqdn()
-  
-  hostname = registry_get_values( 
-    "HKLM\\SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName" ).select { |v| v[:name] == "ComputerName" }[0][:data]
-  domain   = registry_get_values(
+def fqdn_added?( host )
+
+  begin
+    hostname = registry_get_values(
+    "HKLM\\SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ComputerName" ).select { |v| v[:name] == "ComputerName" }[0][:data]
+    domain   = registry_get_values(
     "HKLM\\SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters" ).select { |v| v[:name] == "Domain" }[0][:data]
-  
-  "#{hostname}.#{domain}"  
+
+    host.casecmp( "#{hostname}.#{domain}" ).zero?
+  rescue
+    false
+  end
+end
+
+def fqdn_active?( host )
+
+  return false if !fqdn_added?( host )
+
+  begin
+    hostname = registry_get_values(
+    "HKLM\\SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ActiveComputerName" ).select { |v| v[:name] == "ComputerName" }[0][:data]
+    domain   = registry_get_values(
+    "HKLM\\SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters" ).select { |v| v[:name] == "Domain" }[0][:data]
+
+    host.casecmp( "#{hostname}.#{domain}" ).zero?
+  rescue
+    false
+  end
 end
